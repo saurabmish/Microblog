0d806509e208819579665ef82ff75ea4bd3544e6
Saurabh Mishra <saurab.mish@gmail.com>
2021-01-10 12:51:01 -0800

    ORM for postgres

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
