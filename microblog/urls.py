bf7c85a9427ec0cf9e0ecdd29faa87ec8592d38c
Saurabh Mishra <saurab.mish@gmail.com>
2021-01-11 09:43:10 -0800

    URL more concise for remote deployment

--- microblog/urls.py
+++ microblog/urls.py
@@ -19,7 +19,7 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('microblog/register/', user_views.register, name="register"),
-    path('microblog/', include('app.urls'))
+    path('register/', user_views.register, name="register"),
+    path('', include('app.urls'))
 
 ]
