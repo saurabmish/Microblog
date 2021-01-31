028ed969b64b97d7fca88a45ecfb9e75dd402156
Saurabh Mishra <saurab.mish@gmail.com>
2021-01-31 11:51:09 -0800

    removed unused imports

--- blog/tests/test_blog_views.py
+++ blog/tests/test_blog_views.py
@@ -1,33 +1,29 @@
-import json
-
 from django.test import TestCase, Client
 from django.urls import reverse
-
-from blog.models import Post
 
 class TestBlogViews(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.homepage_url = reverse('homepage')
-        self.aboutpage_url = reverse('about')
+        self.homepage = reverse('homepage')
+        self.aboutpage = reverse('about')
 
     def test_get_homepage_status_code(self):
-        response = self.client.get(self.homepage_url)
+        response = self.client.get(self.homepage)
         self.assertEqual(response.status_code, 200)
 
     def test_get_homepage_rendered_template(self):
-        response = self.client.get(self.homepage_url)
+        response = self.client.get(self.homepage)
         self.assertTemplateUsed(response, 'home.html')
 
     def test_get_aboutpage_status_code(self):
-        response = self.client.get(self.aboutpage_url)
+        response = self.client.get(self.aboutpage)
         self.assertEqual(response.status_code, 200)
 
     def test_get_aboutpage_rendered_template(self):
-        response = self.client.get(self.aboutpage_url)
+        response = self.client.get(self.aboutpage)
         self.assertTemplateUsed(response, 'about.html')
 
     def test_get_aboutpage_title(self):
-        response = self.client.get(self.aboutpage_url)
+        response = self.client.get(self.aboutpage)
         self.assertContains(response, 'About')
