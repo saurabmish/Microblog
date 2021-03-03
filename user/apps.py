f63b71bb1d5cb6a5287063dde9b19d62bf28d51a
Saurabh Mishra <saurab.mish@gmail.com>
2021-03-03 14:27:06 -0800

    import user profile signals

--- user/apps.py
+++ user/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     """The name attribute has to match the directory name."""
     name = 'user'
+
+    def ready(self):
+        import user.signals
